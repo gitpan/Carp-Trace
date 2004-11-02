cfcaf3ea56b12e52e1b0ad9b9b2ac0200212e554
Jos Boumans <kane@cpan.org>
2004-11-02 12:37:06 +0000

    Import of KANE/Carp-Trace-0.10 from CPAN.
    gitpan-cpan-distribution: Carp-Trace
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         KANE/Carp-Trace-0.10.tar.gz
    gitpan-cpan-author:       KANE
    gitpan-cpan-maturity:     released

--- lib/Carp/Trace.pm
+++ lib/Carp/Trace.pm
@@ -1,21 +1,28 @@
 package Carp::Trace;
 use strict;
+use Data::Dumper;
+use Devel::Caller::Perl qw[called_args];
 
 BEGIN {
-    use     vars qw[@ISA @EXPORT $VERSION];
+    use     vars qw[@ISA @EXPORT $VERSION $DEPTH $OFFSET $ARGUMENTS];
     use     Exporter;
     
     @ISA    = 'Exporter';
     @EXPORT = 'trace';
 }
 
-$VERSION = '0.01';
+$OFFSET     = 0;
+$DEPTH      = 0;
+$ARGUMENTS  = 0;
+$VERSION    = '0.10';
 
 sub trace {
-    my $level = shift || 0;
- 
-    my $trace;
-    my $i = 1;
+    my $level   = shift || $DEPTH       || 0;
+    my $offset  = shift || $OFFSET      || 0;
+    my $args    = shift || $ARGUMENTS   || 0;
+
+    my $trace = '';
+    my $i = 1 + $OFFSET;
 
     while (1) {
         last if $level && $level < $i; 
@@ -30,12 +37,25 @@ sub trace {
                     ?   $package . '::' . $subroutine . qq| [$i]| 
                         . (defined $evaltext ? qq[\n\t$evaltext] : '')       
                     :   $subroutine . qq| [$i]|;
+        $string =~ s/\n;$/;/gs;
+        
         $string .= qq[\n\t];
         
         $string .= q[require|use - ] if $is_require;
-        $string .= defined $wantarray ? $wantarray ? 'list - ' : 'scalar - ' : 'void - ';
+        $string .= defined $wantarray 
+                        ? $wantarray ? 'list - ' : 'scalar - ' 
+                        : 'void - ';
         $string .= $hasargs ? 'new stash' : 'no new stash';
         $string .=  qq[\n\t] . $filename . ' line ' . $line . qq[\n];
+    
+        if ($args) {   
+            local $Data::Dumper::Varname    = 'ARGS';
+            local $Data::Dumper::Indent     = 1;
+            
+            for my $line ( split $/, Dumper( called_args($i) ) ) {
+                $string .=  "\t$line\n";
+            }
+        }
     
         $trace = $string . $trace;
     
@@ -63,18 +83,26 @@ Carp::Trace - simple traceback of call stacks
 =head1 DESCRIPTION
 
 Carp::Trace provides an easy way to see the route your script took to
-get to a certain place. It uses simple C<caller> calls to determine this.
+get to a certain place. It uses simple C<caller> calls to determine 
+this.
 
 =head1 FUNCTIONS
 
-=head2 trace( [DEPTH] )
+=head2 trace( [DEPTH, OFFSET, ARGS] )
 
-C<trace> is a function, exported by default, that gives a simple traceback
-of how you got where you are. It returns a formatted string, ready to be
-sent to C<STDOUT> or C<STDERR>.
+C<trace> is a function, exported by default, that gives a simple 
+traceback of how you got where you are. It returns a formatted string, 
+ready to be sent to C<STDOUT> or C<STDERR>.
 
-Optionally, you can provide a DEPTH argument, which tells trace to only
-go back so many levels.
+Optionally, you can provide a DEPTH argument, which tells C<trace> to 
+only go back so many levels. The OFFSET argument will tell C<trace> to 
+skip the first [OFFSET] layers up.
+
+If you provide a true value for the C<ARGS> parameter, the arguments 
+passed to each callstack will be dumped using C<Data::Dumper>.
+This might slow down your trace, but is very useful for debugging.
+
+See also the L<Global Variables> section.
 
 C<trace> is able to tell you the following things:
 
@@ -144,9 +172,27 @@ Might look something like this:
         list - new stash
         x.pl line 1
 
+=head1 Global Variables
+
+=head2 $Carp::Trace::DEPTH
+
+Sets the depth to be used by default for C<trace>. Any depth argument
+to C<trace> will override this setting.
+
+=head2 $Carp::Trace::OFFSET
+
+Sets the offset to be used by default for C<trace>. Any offset
+argument to C<trace> will override this setting.
+
+=head2 $Carp::Trace::ARGUMENTS
+
+Sets a flag to indicate that a C<trace> should dump all arguments for
+every call stack it's printing out. Any C<args> argument to C<trace> 
+will override this setting.
+
 =head1 AUTHOR
 
-his module by
+This module by
 Jos Boumans E<lt>kane@cpan.orgE<gt>.
 
 =head1 COPYRIGHT
--- t/00_trace.t
+++ t/00_trace.t
@@ -1,6 +1,6 @@
 use lib 'lib';
-use Test::More tests => 2;
-
+use Test::More tests => 3;
+BEGIN { $^W = 0 }
 use_ok('Carp::Trace');
 
 sub foo { bar() };
@@ -8,14 +8,14 @@ sub bar { $x = baz() };
 sub baz { @y = zot() };
 sub zot { $z = trace() };
 
-eval 'foo(1)';
+### trace without args
+{   eval 'foo(1)';
 
 my $expect = 
 qq|main::(eval) [5]
-\tfoo(1)
-;
+\tfoo(1);
 \tvoid - no new stash
-\tt/00_trace.t line 11
+\tt/00_trace.t line 12
 main::foo [4]
 \tvoid - new stash
 \t(eval 2) line 1
@@ -29,5 +29,36 @@ main::zot [1]
 \tlist - new stash
 \tt/00_trace.t line 8
 |;
+    
+    is($z, $expect, "Trace with no arguments");
+}
 
-is($z, $expect, "Trace");
+### Trace with args
+{   local $Carp::Trace::ARGUMENTS = 1;
+    eval foo($0,[1]);
+
+my $expect = 
+qq|main::foo [4]
+\tscalar - new stash
+\tt/00_trace.t line 38
+\t\$ARGS1 = 't/00_trace.t';
+\t\$ARGS2 = [
+\t  1
+\t];
+main::bar [3]
+\tscalar - new stash
+\tt/00_trace.t line 6
+\t\$ARGS1 = 't/00_trace.t';
+\t\$ARGS2 = [
+\t  1
+\t];
+main::baz [2]
+\tscalar - new stash
+\tt/00_trace.t line 7
+main::zot [1]
+\tlist - new stash
+\tt/00_trace.t line 8
+|;
+    
+    is($z, $expect, "Trace with arguments");
+}
